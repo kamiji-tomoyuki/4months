bce4489f68e1644068cb18b7da91cf8b33192587
NagaiKohaku <k023g0037@g.neec.ac.jp>
2025-03-31 16:18:19 +0900

    パーティクルマネージャーにコメントを追加

--- engine/3d/particle/ParticleManager.cpp
+++ engine/3d/particle/ParticleManager.cpp
@@ -4,38 +4,65 @@
 #include "filesystem"
 #include "Vector3.h"
 
+// モデルキャッシュ
 std::unordered_map<std::string, ParticleManager::ModelData> ParticleManager::modelCache;
 
+// シングルトンインスタンス
 ParticleManager* ParticleManager::instance = nullptr;
 
 ParticleManager* ParticleManager::GetInstance() {
+
+	// インスタンスがない場合
 	if (instance == nullptr) {
+
+		// インスタンスを生成
 		instance = new ParticleManager();
 	}
+
+	// インスタンスを返す
 	return instance;
 }
 
 void ParticleManager::Finalize() {
+
+	// インスタンスを削除
 	delete instance;
+
+	// インスタンスをnullptrで初期化
 	instance = nullptr;
 }
 
 void ParticleManager::Initialize(SrvManager* srvManager) {
+
+	// パーティクル共通の取得
 	particleCommon = ParticleCommon::GetInstance();
+
+	// シェーダーリソースビューマネージャの取得
 	srvManager_ = srvManager;
+
+	// シード値の生成
 	randomEngine.seed(seedGenerator());
 
+	// モデルファイル名の取得
 	for (const auto& entry : std::filesystem::directory_iterator(kDirectoryPath)) {
+
+		// objファイルのみ取得
 		if (entry.path().extension() == ".obj") {
+
+			// ファイル名を追加
 			modelFiles.push_back(entry.path().filename().string());
 		}
 	}
 }
 
 void ParticleManager::Update(const ViewProjection& viewProjection) {
-	// --- 各行列の初期化・計算 ---
+
+	/// === 行列の生成 === ///
+
+	// ビュープロジェクション行列の生成
 	Matrix4x4 viewProjectionMatrix = viewProjection.matView_ * viewProjection.matProjection_;
 
+	// ビルボード行列の生成
 	Matrix4x4 billboardMatrix = viewProjection.matView_;
 	billboardMatrix.m[3][0] = 0.0f;
 	billboardMatrix.m[3][1] = 0.0f;
@@ -44,17 +71,24 @@ void ParticleManager::Update(const ViewProjection& viewProjection) {
 
 	billboardMatrix = Inverse(billboardMatrix);
 
+	/// === パーティクルの更新 === ///
+
+	// パーティクルグループごとに更新
 	for (auto& [groupName, particleGroup] : particleGroups) {
 
+		// パーティクルの合計生成数
 		uint32_t numInstance = 0;
 
-		// --- パーティクルの更新 ---
+		// パーティクルごとに更新
 		for (auto particleIterator = particleGroup.particles.begin();
 			particleIterator != particleGroup.particles.end();) {
 
 			/// === 削除処理 === ///
 
+			// ライフタイムを超えたパーティクルの削除
 			if (particleIterator->lifeTime <= particleIterator->currentTime) {
+
+				// パーティクルの削除
 				particleIterator = particleGroup.particles.erase(particleIterator);
 				continue;
 			}
@@ -67,6 +101,7 @@ void ParticleManager::Update(const ViewProjection& viewProjection) {
 
 			/// === 移動処理 === ///
 
+			// パーティクルの座標更新
 			particleIterator->transform.translation_ = UpdateParameter(
 				particleIterator->positionPara,
 				particleIterator->positionState,
@@ -77,6 +112,7 @@ void ParticleManager::Update(const ViewProjection& viewProjection) {
 
 			/// === 回転処理 === ///
 
+			// パーティクルの回転更新
 			particleIterator->transform.rotation_ = UpdateParameter(
 				particleIterator->rotationPara,
 				particleIterator->rotationState,
@@ -87,6 +123,7 @@ void ParticleManager::Update(const ViewProjection& viewProjection) {
 
 			/// === 拡縮処理 === ///
 
+			// パーティクルの拡縮更新
 			particleIterator->transform.scale_ = UpdateParameter(
 				particleIterator->scalePara,
 				particleIterator->scaleState,
@@ -97,50 +134,73 @@ void ParticleManager::Update(const ViewProjection& viewProjection) {
 
 			/// === 色 === ///
 
+			// パーティクルの色更新
 			particleIterator->color = Lerp(particleIterator->startColor, particleIterator->endColor, t);
+
+			/// === ワールド座標の更新 === ///
 
 			// ワールドトランスフォームの更新
 			particleIterator->transform.UpdateMatrix();
 
+			// パーティクルのワールド座標にエミッターのワールド座標を掛ける
 			particleIterator->transform.matWorld_ *= particleIterator->emitterTransform.matWorld_;
 
+			// ワールド座標の送信
 			particleIterator->transform.TransferMatrix();
 
 			// ワールド行列の計算
 			Matrix4x4 worldMatrix{};
 
+			// ビルボード処理が必要な場合
 			if (particleIterator->isBillboard) {
 
+				// パーティクルの座標取得
 				Vector3 objPos = {
 					particleIterator->transform.matWorld_.m[3][0],
 					particleIterator->transform.matWorld_.m[3][1],
 					particleIterator->transform.matWorld_.m[3][2]
 				};
 
+				// ビルボード行列の計算
 				worldMatrix =
 					MakeScaleMatrix(particleIterator->transform.scale_) *
 					billboardMatrix *
 					MakeTranslateMatrix(objPos);
 
 			} else {
-				// 通常
+
+				// ビルボードなしのワールド行列の計算
 				worldMatrix = particleIterator->transform.matWorld_;
 			}
 
 			// ワールド・ビュー・プロジェクション行列計算
 			Matrix4x4 worldViewProjectionMatrix = worldMatrix * viewProjectionMatrix;
 
-			// インスタンスデータ設定
+			/// === インスタンスデータ設定 === ///
+
+			// インスタンス数が最大数未満の場合
 			if (numInstance < kNumMaxInstance) {
+
+				// WVPの設定
 				particleGroup.instancingData[numInstance].WVP = worldViewProjectionMatrix;
+
+				// ワールド座標の設定
 				particleGroup.instancingData[numInstance].World = worldMatrix;
+
+				// 色の設定
 				particleGroup.instancingData[numInstance].color = (*particleIterator).color;
+
+				// アルファ値の設定
 				particleGroup.instancingData[numInstance].color.w = (*particleIterator).color.w;
+
+				// 生成数の更新
 				++numInstance;
 			}
 
+			// パーティクルの時間更新
 			particleIterator->currentTime += kDeltaTime;
 
+			// 次のパーティクルへ
 			++particleIterator;
 		}
 
@@ -151,8 +211,12 @@ void ParticleManager::Update(const ViewProjection& viewProjection) {
 
 void ParticleManager::Draw() {
 
+	// パーティクルグループごとに描画
 	for (auto& [groupName, particleGroup] : particleGroups) {
+
+		// インスタンス数が0より大きい場合
 		if (particleGroup.instanceCount > 0) {
+
 			particleCommon->GetDxCommon()->GetCommandList()->IASetVertexBuffers(0, 1, &particleGroup.vertexBufferView);
 
 			particleCommon->GetDxCommon()->GetCommandList()->SetGraphicsRootConstantBufferView(0, particleGroup.materialResource->GetGPUVirtualAddress());
@@ -186,14 +250,19 @@ std::list<ParticleManager::Particle> ParticleManager::Emit(
 	const bool& isBillboard
 ) {
 
+	// 指定された名前のパーティクルグループが存在するか確認
 	assert(particleGroups.find(name) != particleGroups.end() && "Error: パーティクルグループが存在しません。");
 
+	// パーティクルグループの取得
 	ParticleGroup& particleGroup = particleGroups[name];
 
+	// 新しいパーティクルのリスト
 	std::list<Particle> newParticles;
 
+	// 新しいパーティクルの生成
 	if (particleGroup.instanceCount <= static_cast<uint32_t>(count)) {
 
+		// パーティクルの生成
 		Particle particle = MakeNewParticle(
 			randomEngine,
 			parent,
@@ -213,9 +282,11 @@ std::list<ParticleManager::Particle> ParticleManager::Emit(
 			isBillboard
 		);
 
+		// パーティクルリストに追加
 		newParticles.push_back(particle);
 	}
 
+	// パーティクルグループのパーティクルリストに追加
 	particleGroup.particles.splice(particleGroup.particles.end(), newParticles);
 
 	return newParticles;
@@ -322,30 +393,38 @@ ParticleManager::Particle ParticleManager::MakeNewParticle(
 
 Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& state, EasingState& easingState, float lifeTime, float t) {
 
+	// イージング処理の初期値
 	Vector3 startPos;
 
+	// イージング処理の終了値
 	Vector3 endPos;
 
+	// 結果
 	Vector3 result;
 
 	switch (state) {
 
 	case START:
 
+		// パラメータの開始値をそのまま設定
 		result = parameter.startNum;
 
 		break;
 
 	case VELOCITY:
 
+		// パラメータの速度を加算
 		parameter.velocity += parameter.acceleration;
 
+		// イージング処理の開始値をパラメータの開始値で設定
 		startPos = parameter.startNum;
 
+		// イージング処理の終了値を移動処理後の値で設定
 		endPos = parameter.startNum +
 			parameter.velocity *
 			(lifeTime / kDeltaTime);
 
+		// イージング処理
 		result =
 			Lerp(
 				startPos,
@@ -357,10 +436,12 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 	case EASING:
 
+		// イージング状態で処理を分岐
 		switch (easingState) {
 
 		case LERP:
 
+			// 線形補間
 			result =
 				Lerp(
 					parameter.startNum,
@@ -372,6 +453,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEINSINE:
 
+			// EaseIn(等倍)
 			result =
 				EaseIn(
 					parameter.startNum,
@@ -384,6 +466,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEINQUAD:
 
+			// EaseIn(2乗)
 			result =
 				EaseIn(
 					parameter.startNum,
@@ -396,6 +479,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEINCUBIC:
 
+			// EaseIn(3乗)
 			result =
 				EaseIn(
 					parameter.startNum,
@@ -408,6 +492,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEINQUART:
 
+			// EaseIn(4乗)
 			result =
 				EaseIn(
 					parameter.startNum,
@@ -420,6 +505,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEOUTSINE:
 
+			// EaseOut(等倍)
 			result =
 				EaseOut(
 					parameter.startNum,
@@ -432,6 +518,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEOUTQUAD:
 
+			// EaseOut(2乗)
 			result =
 				EaseOut(
 					parameter.startNum,
@@ -444,6 +531,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEOUTCUBIC:
 
+			// EaseOut(3乗)
 			result =
 				EaseOut(
 					parameter.startNum,
@@ -456,6 +544,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEOUTQUART:
 
+			// EaseOut(4乗)
 			result =
 				EaseOut(
 					parameter.startNum,
@@ -468,6 +557,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEINOUTSINE:
 
+			// EaseInOut(等倍)
 			result =
 				EaseInOut(
 					parameter.startNum,
@@ -480,6 +570,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEINOUTQUAD:
 
+			// EaseInOut(2乗)
 			result =
 				EaseInOut(
 					parameter.startNum,
@@ -492,6 +583,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEINOUTCUBIC:
 
+			// EaseInOut(3乗)
 			result =
 				EaseInOut(
 					parameter.startNum,
@@ -504,6 +596,7 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 
 		case EASEINOUTQUART:
 
+			// EaseInOut(4乗)
 			result =
 				EaseInOut(
 					parameter.startNum,
@@ -522,26 +615,33 @@ Vector3 ParticleManager::UpdateParameter(Parameter& parameter, ParameterState& s
 	return result;
 }
 
-void ParticleManager::CreateParticleGroup(std::string& name, const std::string& filename) {
-	// --- パーティクルグループ生成 ---
+void ParticleManager::CreateParticleGroup(std::string& name, const std::string& modelFileName) {
 
+	// グループ名重複時のID
 	int id = 0;
 
+	// グループ名を取得
 	std::string groupName = name;
 
+	// グループ名が重複している場合
 	while (particleGroups.contains(groupName)) {
 
+		// IDをインクリメント
 		id++;
 
+		// グループ名を再設定
 		groupName = name + std::to_string(id);
 	}
 
+	// 再設定した名前をグループ名として設定
 	name = groupName;
 
+	// パーティクルグループの生成
 	particleGroups[groupName] = ParticleGroup();
 	ParticleGroup& particleGroup = particleGroups[groupName];
 
-	particleGroup.modelData = LoadObjFile(kDirectoryPath, filename);
+	// --- モデルデータ生成 ---
+	particleGroup.modelData = LoadObjFile(kDirectoryPath, modelFileName);
 
 	// --- 頂点リソース生成 ---
 	particleGroup.vertexResource = particleCommon->GetDxCommon()->CreateBufferResource(sizeof(VertexData) * particleGroup.modelData.vertices.size());
@@ -555,13 +655,13 @@ void ParticleManager::CreateParticleGroup(std::string& name, const std::string& 
 	particleGroup.vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&particleGroup.vertexData));
 	std::memcpy(particleGroup.vertexData, particleGroup.modelData.vertices.data(), sizeof(VertexData) * particleGroup.modelData.vertices.size());
 
+	// --- テクスチャ読み込み ---
 	TextureManager::GetInstance()->LoadTexture(particleGroup.modelData.material.textureFilePath);
 
+	// --- インスタンスリソース生成 ---
 	particleGroup.instancingResource = particleCommon->GetDxCommon()->CreateBufferResource(sizeof(ParticleForGPU) * kNumMaxInstance);
-
 	particleGroup.instancingSRVIndex = srvManager_->Allocate() + 1;
 	particleGroup.instancingResource->Map(0, nullptr, reinterpret_cast<void**>(&particleGroup.instancingData));
-
 	srvManager_->CreateSRVforStructuredBuffer(particleGroup.instancingSRVIndex, particleGroup.instancingResource.Get(), kNumMaxInstance, sizeof(ParticleForGPU));
 
 	// --- マテリアルリソース生成 ---
@@ -570,40 +670,50 @@ void ParticleManager::CreateParticleGroup(std::string& name, const std::string& 
 	particleGroup.materialData->color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
 	particleGroup.materialData->uvTransform = MakeIdentity4x4();
 
+	// --- インスタンス数初期化 ---
 	particleGroup.instanceCount = 0;
 }
 
 void ParticleManager::ChangeGroupName(const std::string& name, const std::string& preName) {
 
+	// 旧名が存在しない場合
 	if (!particleGroups.contains(preName)) {
 		return;
 	}
 
+	// 新名のパーティクルグループに旧名のパーティクルグループを移動
 	particleGroups[name] = particleGroups[preName];
 
+	// 旧名のパーティクルグループを削除
 	particleGroups.erase(preName);
 }
 
-void ParticleManager::ReloadGroup(std::string& name, const std::string& preName, const std::string& filename) {
+void ParticleManager::ReloadGroup(std::string& name, const std::string& preName, const std::string& modelFileName) {
 
+	// 旧名が存在しない場合
 	if (!particleGroups.contains(preName)) {
 		return;
 	}
 
+	// 旧名のパーティクルグループを削除
 	particleGroups.erase(preName);
 
-	CreateParticleGroup(name, filename);
+	// パーティクルグループの生成
+	CreateParticleGroup(name, modelFileName);
 }
 
 void ParticleManager::ClearParticles() {
 
+	// パーティクルグループの全削除
 	particleGroups.clear();
 }
 
 std::vector<const char*> ParticleManager::GetModelFiles() {
 
+	// モデルファイル名を返す
 	std::vector<const char*> items;
 
+	// モデルファイル名を追加
 	for (const auto& file : modelFiles) {
 		items.push_back(file.c_str());
 	}
@@ -611,41 +721,48 @@ std::vector<const char*> ParticleManager::GetModelFiles() {
 	return items;
 }
 
-void ParticleManager::CreateVartexData(const std::string& filename) {
-	//modelData = LoadObjFile(kDirectoryPath, filename);
+ParticleManager::MaterialData ParticleManager::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& modelFileName) {
 
-	//// --- 頂点リソース生成 ---
-	//vertexResource = particleCommon->GetDxCommon()->CreateBufferResource(sizeof(VertexData) * modelData.vertices.size());
-	//// --- 頂点バッファビュー生成 ---
-	//vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-	//vertexBufferView.SizeInBytes = UINT(sizeof(VertexData) * modelData.vertices.size());
-	//vertexBufferView.StrideInBytes = sizeof(VertexData);
-
-	//// --- 書き込み ---
-	//vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	//std::memcpy(vertexData, modelData.vertices.data(), sizeof(VertexData) * modelData.vertices.size());
-}
-
-ParticleManager::MaterialData ParticleManager::LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename) {
+	// マテリアルデータ
 	MaterialData materialData;
+
+	// ファイル1行分の文字列
 	std::string line;
 
-	std::ifstream file(directoryPath + "/" + filename);
+	// ファイルパスを設定
+	std::ifstream file(directoryPath + "/" + modelFileName);
+
+	// ファイルを開く
 	assert(file.is_open());
 
+	// ファイルの内容を1行ずつ読み込む
 	while (std::getline(file, line)) {
+
+		// 識別子
 		std::string identifier;
+
+		// 1行を読み込む
 		std::istringstream s(line);
+
+		// 識別子を取得
 		s >> identifier;
 
+		// 識別子が"map_Kd"の場合
 		if (identifier == "map_Kd") {
+
+			// テクスチャファイル名を取得
 			std::string textureFilename;
 			s >> textureFilename;
+
+			// テクスチャファイルパスを設定
 			materialData.textureFilePath = directoryPath + "/" + textureFilename;
 		}
 	}
 
+	// テクスチャファイルパス指定されていない場合
 	if (materialData.textureFilePath.empty()) {
+
+		// テクスチャファイルパスを白色の1x1テクスチャに設定
 		materialData.textureFilePath = directoryPath + "/../images/white1x1.png";
 	}
 
@@ -653,86 +770,170 @@ ParticleManager::MaterialData ParticleManager::LoadMaterialTemplateFile(const st
 }
 
 
-ParticleManager::ModelData ParticleManager::LoadObjFile(const std::string& directoryPath, const std::string& filename) {
-	// --- .obj読み込み ---
-	std::string fullPath = directoryPath + filename;
+ParticleManager::ModelData ParticleManager::LoadObjFile(const std::string& directoryPath, const std::string& modelFileName) {
 
+	// フルパスの取得
+	std::string fullPath = directoryPath + modelFileName;
+
+	// モデルキャッシュから検索
 	auto it = modelCache.find(fullPath);
+
+	// モデルキャッシュに存在する場合
 	if (it != modelCache.end()) {
+
+		// キャッシュから取得
 		return it->second;
 	}
 
+	// モデルデータ
 	ModelData modelData;
+
+	// 頂点データ
 	std::vector<Vector4> positions;
+
+	// テクスチャ座標データ
 	std::vector<Vector2> texcoords;
+
+	// ファイル1行分の文字列
 	std::string line;
 
+	// ファイルパスを設定
 	std::string folderPath;
-	size_t lastSlashPos = filename.find_last_of("/\\");
+
+	// ファイル名の部分のスラッシュ位置を取得
+	size_t lastSlashPos = modelFileName.find_last_of("/\\");
+
+	// 値が見つかった場合
 	if (lastSlashPos != std::string::npos) {
-		folderPath = filename.substr(0, lastSlashPos);
+
+		// フォルダパスを取得
+		folderPath = modelFileName.substr(0, lastSlashPos);
 	}
 
+	// ファイルパスを設定
 	std::ifstream file(fullPath);
+
+	// ファイルを開く
 	assert(file.is_open());
 
+	// ファイルの内容を1行ずつ読み込む
 	while (std::getline(file, line)) {
+
+		// 識別子
 		std::string identifier;
+
+		// 1行を読み込む
 		std::istringstream s(line);
+
+		// 識別子を取得
 		s >> identifier;
 
 		if (identifier == "v") {
+
+			/// === 識別子が「v」の場合 === ///
+
+			// 頂点座標
 			Vector4 position;
+
+			// 頂点座標を取得
 			s >> position.x >> position.y >> position.z;
+
+			// X軸を反転
 			position.x *= -1.0f;
+
+			// W座標を設定
 			position.w = 1.0f;
+
+			// 頂点座標をデータに追加
 			positions.push_back(position);
+
 		} else if (identifier == "vt") {
+
+			/// === 識別子が「vt」の場合 === ///
+
+			// テクスチャ座標
 			Vector2 texcoord;
+
+			// テクスチャ座標を取得
 			s >> texcoord.x >> texcoord.y;
+
+			// Y軸座標を調整
 			texcoord.y = 1.0f - texcoord.y;
+
+			// テクスチャ座標をデータに追加
 			texcoords.push_back(texcoord);
+
 		} else if (identifier == "f") {
+
+			/// === 識別子が「f」の場合 === ///
+
+			// 面の頂点データ
 			VertexData triangle[3];
+
+			// 面の頂点データを取得
 			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
+
+				// 頂点定義
 				std::string vertexDefinition;
+
+				// 頂点定義を取得
 				s >> vertexDefinition;
+
+				// 頂点定義をストリームに変換
 				std::istringstream v(vertexDefinition);
+
+				// 頂点データ
 				uint32_t elementIndices[3];
+
+				// 頂点データを取得
 				for (int32_t element = 0; element < 3; ++element) {
 					std::string index;
 					std::getline(v, index, '/');
 					elementIndices[element] = std::stoi(index);
 				}
+
+				// 座標データを取得
 				Vector4 position = positions[elementIndices[0] - 1];
+
+				// テクスチャ座標データを取得
 				Vector2 texcoord = texcoords[elementIndices[1] - 1];
+
+				// 頂点データを作成
 				VertexData vertex = { position, texcoord };
+
+				// 頂点データをデータに追加
 				modelData.vertices.push_back(vertex);
+
+				// 三角形データに追加
 				triangle[faceVertex] = { position, texcoord };
 			}
 		} else if (identifier == "mtllib") {
+
+			/// === 識別子が「mtllib」の場合 === ///
+
+			// マテリアルファイル名
 			std::string materialFilename;
+
+			// マテリアルファイル名を取得
 			s >> materialFilename;
+
+			// フォルダパスが存在する場合
 			if (!folderPath.empty()) {
+
+				// フォルダパスからマテリアルファイルを読み込む
 				modelData.material = LoadMaterialTemplateFile(directoryPath + folderPath, materialFilename);
 			} else {
+
+				// マテリアルファイルを読み込む
 				modelData.material = LoadMaterialTemplateFile(directoryPath, materialFilename);
 			}
 		}
 	}
 
+	// モデルキャッシュに追加
 	modelCache[fullPath] = modelData;
 
 	return modelData;
-}
-
-
-void ParticleManager::CreateMaterial() {
-	// --- マテリアルリソース生成 ---
-	//materialResource = particleCommon->GetDxCommon()->CreateBufferResource(sizeof(Material));
-	//materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
-	//materialData->color = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
-	//materialData->uvTransform = MakeIdentity4x4();
 }
 
 Vector4 ParticleManager::DistributionVector4(Vector4 num, Vector4 range) {
--- engine/3d/particle/ParticleManager.h
+++ engine/3d/particle/ParticleManager.h
@@ -16,10 +16,15 @@
 class ParticleManager {
 public:
 
+	// シングルトンインスタンス
 	static ParticleManager* instance;
 
+	// シングルトンインスタンスの取得
 	static ParticleManager* GetInstance();
 
+	/// <summary>
+	/// 終了処理
+	/// </summary>
 	void Finalize();
 
 	/// <summary>
@@ -38,26 +43,37 @@ public:
 	void Draw();
 
 	/// <summary>
-	/// パーティクルグループ生成
+	/// パーティクルグループの生成
 	/// </summary>
-	/// <param name="name"></param>
-	/// <param name="textureFilePath"></param>
-	void CreateParticleGroup(std::string& name, const std::string& filename);
-
-	void ChangeGroupName(const std::string& name, const std::string& preName);
-
-	void ReloadGroup(std::string& name, const std::string& preName, const std::string& filename);
-
-	void ClearParticles();
-
-	std::vector<const char*> GetModelFiles();
-
-private:
+	/// <param name="name">グループ名</param>
+	/// <param name="modelFileName">モデルのファイル名</param>
+	void CreateParticleGroup(std::string& name, const std::string& modelFileName);
 
 	/// <summary>
-	/// 頂点データ作成
+	/// パーティクルグループの名前変更
 	/// </summary>
-	void CreateVartexData(const std::string& filename);
+	/// <param name="name">変更後の名前</param>
+	/// <param name="preName">変更前の名前</param>
+	void ChangeGroupName(const std::string& name, const std::string& preName);
+
+	/// <summary>
+	/// パーティクルグループの再生成
+	/// </summary>
+	/// <param name="name">再生成後の名前</param>
+	/// <param name="preName">再生成前の名前</param>
+	/// <param name="modelFileName">モデルのファイル名</param>
+	void ReloadGroup(std::string& name, const std::string& preName, const std::string& modelFileName);
+
+	/// <summary>
+	/// パーティクルグループの全削除
+	/// </summary>
+	void ClearParticles();
+
+	/// <summary>
+	/// モデルのファイル名取得
+	/// </summary>
+	/// <returns></returns>
+	std::vector<const char*> GetModelFiles();
 
 private:
 
@@ -74,6 +90,7 @@ private:
 		float padding[3];
 	};
 
+	// --- マテリアルのファイル名 ---
 	struct MaterialData {
 		std::string textureFilePath;
 	};
@@ -84,85 +101,96 @@ private:
 		MaterialData material;
 	};
 
+	// --- パーティクルのGPUデータ ---
 	struct ParticleForGPU {
 		Matrix4x4 WVP;
 		Matrix4x4 World;
 		Vector4 color;
 	};
 
+	// --- パーティクル ---
 	struct Particle {
+		/// === ワールド座標 === ///
 		WorldTransform transform;
 		WorldTransform emitterTransform;
+		/// === 座標のパラメータ === ///
 		ParameterState positionState;
 		EasingState positionEasingState;
 		Parameter positionPara;
+		/// === 回転のパラメータ === ///
 		ParameterState rotationState;
 		EasingState rotationEasingState;
 		Parameter rotationPara;
+		/// === 拡縮のパラメータ === ///
 		ParameterState scaleState;
 		EasingState scaleEasingState;
 		Parameter scalePara;
+		/// === 色 === ///
 		Vector4 color;
 		Vector4 startColor;
 		Vector4 endColor;
 		Vector4 randomRangeColor;
+		/// === タイマー === ///
 		float lifeTime;
 		float currentTime;
+		/// === その他の項目 === ///
 		bool isBillboard;
 	};
 
-	//struct ParticleGroup {
-	//	MaterialData material;
-	//	std::list<Particle> particles;
-	//	uint32_t instancingSRVIndex = 0;
-	//	Microsoft::WRL::ComPtr<ID3D12Resource> instancingResource = nullptr;
-	//	uint32_t instanceCount = 0;
-	//	ParticleForGPU* instancingData = nullptr;
-	//};
-
+	// --- パーティクルグループ ---
 	struct ParticleGroup {
+		/// === パーティクル === ///
 		std::list<Particle> particles;
+		/// === インスタンスデータ === ///
 		Microsoft::WRL::ComPtr<ID3D12Resource> instancingResource = nullptr;
 		ParticleForGPU* instancingData = nullptr;
 		uint32_t instancingSRVIndex = 0;
 		uint32_t instanceCount = 0;
+		/// === 頂点データ === ///
 		Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource = nullptr;
 		VertexData* vertexData = nullptr;
 		D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
+		/// === マテリアルデータ === ///
 		Microsoft::WRL::ComPtr<ID3D12Resource> materialResource = nullptr;
 		Material* materialData = nullptr;
 		ModelData modelData;
 	};
 
+	// パーティクル共通
 	ParticleCommon* particleCommon = nullptr;
+
+	// シェーダーリソースビューマネージャ
 	SrvManager* srvManager_;
 
-	//Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource = nullptr;
-	//VertexData* vertexData = nullptr;
-	//D3D12_VERTEX_BUFFER_VIEW vertexBufferView;
-
-	//Microsoft::WRL::ComPtr<ID3D12Resource> materialResource = nullptr;
-	//Material* materialData = nullptr;
-
-	//ModelData modelData;
-
+	// モデルキャッシュ
 	static std::unordered_map<std::string, ModelData> modelCache;
 
+	// モデルファイル名
 	std::vector<std::string> modelFiles;
 
-	std::unordered_map<std::string, ParticleGroup>particleGroups;
+	// パーティクルグループ
+	std::unordered_map<std::string, ParticleGroup> particleGroups;
 
 	// デルタタイム
 	const float kDeltaTime = 1.0f / 60.0f;
+
+	// 最大インスタンス数
 	static const uint32_t kNumMaxInstance = 10000;
 
+	// 乱数のシード値
 	std::random_device seedGenerator;
+
+	// 乱数エンジン
 	std::mt19937 randomEngine;
 
+	// ディレクトリのパス
 	const std::string kDirectoryPath = "resources/models/GameScene/";
 
 public:
 
+	/// <summary>
+	/// パーティクルの発生
+	/// </summary>
 	std::list<Particle> Emit(
 		const std::string name,
 		const WorldTransform& parent,
@@ -188,24 +216,28 @@ private:
 	/// <summary>
 	/// .mtlファイルの読み取り
 	/// </summary>
-	/// <param name="directoryPath"></param>
-	/// <param name="filename"></param>
-	/// <returns></returns>
-	static MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename);
+	/// <param name="directoryPath">ディレクトリパス</param>
+	/// <param name="modelFileName">ファイル名</param>
+	/// <returns>マテリアルデータ</returns>
+	static MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& modelFileName);
 
 	/// <summary>
 	///  .objファイルの読み取り
 	/// </summary>
-	/// <param name="directoryPath"></param>
-	/// <param name="filename"></param>
-	/// <returns></returns>
-	static ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename);
+	/// <param name="directoryPath">ディレクトリパス</param>
+	/// <param name="filename">ファイル名</param>
+	/// <returns>モデルデータ</returns>
+	static ModelData LoadObjFile(const std::string& directoryPath, const std::string& modelFileName);
 
 	/// <summary>
 	/// マテリアルデータ作成
 	/// </summary>
 	void CreateMaterial();
 
+	/// <summary>
+	/// パーティクルの生成
+	/// </summary>
+	/// <returns>パーティクル</returns>
 	Particle MakeNewParticle(
 		std::mt19937& randomEngine,
 		const WorldTransform& parent,
@@ -225,11 +257,38 @@ private:
 		const bool& isBillboard
 	);
 
+	/// <summary>
+	/// パーティクルパラメータの更新
+	/// </summary>
+	/// <param name="para">パラメータ</param>
+	/// <param name="state">状態</param>
+	/// <param name="easingState">イージング状態</param>
+	/// <param name="lifeTime">生存時間</param>
+	/// <param name="t">現在時間</param>
+	/// <returns>パラメータ更新の値</returns>
 	Vector3 UpdateParameter(Parameter& para, ParameterState& state, EasingState& easingState, float lifeTime, float t);
 
+	/// <summary>
+	/// ランダム値の生成(Vector4)
+	/// </summary>
+	/// <param name="num">基準値</param>
+	/// <param name="range">ランダム幅</param>
+	/// <returns>生成した値</returns>
 	Vector4 DistributionVector4(Vector4 num, Vector4 range);
 
+	/// <summary>
+	/// ランダム値の生成(Vector3)
+	/// </summary>
+	/// <param name="num">基準値</param>
+	/// <param name="range">ランダム幅</param>
+	/// <returns>生成した値</returns>
 	Vector3 DistributionVector3(Vector3 num, Vector3 range);
 
+	/// <summary>
+	/// ランダム値の生成(float)
+	/// </summary>
+	/// <param name="num">基準値</param>
+	/// <param name="range">ランダム幅</param>
+	/// <returns>生成した値</returns>
 	float DistributionFloat(float num, float range);
 };
