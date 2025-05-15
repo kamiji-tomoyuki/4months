50056d62de906058a747c2f523962acb29358fd7
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2025-05-15 17:05:21 +0900

    当たり判定の修正

--- application/Enemy/Boss/BossStateAttack.cpp
+++ application/Enemy/Boss/BossStateAttack.cpp
@@ -15,7 +15,6 @@ void BossStateAttack::Initialize(){
 	Vector3 newRotate = { pi_v<float> *0.5f, 0.0f,0.0f};
 	quaternion_ = Quaternion::MakeRotateAxisAngleQuaternion(Vector3(0.0f, 1.0f, 0.0f), pi_v<float> *0.5f);
 	quaternion_ = quaternion_ * Quaternion::MakeRotateAxisAngleQuaternion(Vector3(0.0f, 0.0f, 1.0f), pi_v<float> *0.5f);
-	//quaternion_ = quaternion_ * Quaternion::MakeRotateAxisAngleQuaternion(Vector3(0.0f, 0.0f, 1.0f), newRotate.z);
 
 	newRotate = quaternion_.ToEulerAngles();
 	boss_->GetSword()->SetRotation(newRotate);
@@ -30,10 +29,10 @@ void BossStateAttack::Update(){
 	Vector3 newRotate = boss_->GetSword()->GetRotate();
 	quaternion_ = Quaternion::MakeRotateAxisAngleQuaternion(Vector3(0.0f, 1.0f, 0.0f), theta) * quaternion_;
 	newRotate = quaternion_.ToEulerAngles();
-	//boss_->GetSword()->SetRotation(newRotate);
+	boss_->GetSword()->SetRotation(newRotate);
 	// 回転
 	newPos = Transformation(newPos, MakeRotateYMatrix(theta));
-	//boss_->GetSword()->SetTranslation(newPos);
+	boss_->GetSword()->SetTranslation(newPos);
 
 	////プレイヤーの位置によって行動を変える
 	//if (Vector3(player_->GetCenterPosition() - enemy_->GetCenterPosition()).Length() < enemy_->GetShortDistance()) {
